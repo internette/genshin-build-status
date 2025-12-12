b418b1d90c3ac774d6dedb2bcd0c20e4c3b05c04
Antoinette Janus <antoinettejanus@Antoinettes-Laptop.local>
2025-12-12 15:10:50 -0800

    Set up front end to read use context

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+import { useContext } from "react";
 import UuidInputForm from "./components/uuidInputForm/uuidInputForm";
-import { UserSessionProvider } from "./contexts/userSession/userSession";
+import { UserSessionContext } from "./contexts/userSession/userSession";
 export default function Home() {
-  
+  const { userSessionInfo } = useContext(UserSessionContext);
+  console.log("userSessionInfo in page.tsx:", userSessionInfo);
   return (
-    <UserSessionProvider>
+    <>{
+      userSessionInfo.uuid ? 
+      <p>Your UUID is: {userSessionInfo.uuid}</p> :
       <UuidInputForm />
-    </UserSessionProvider>
+    }</>
   );
 }
