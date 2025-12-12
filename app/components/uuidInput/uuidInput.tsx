a6eeb29609499db04596a7d003c78c95d4ffead8
Antoinette Janus <antoinettejanus@Antoinettes-Laptop.local>
2025-12-12 14:34:28 -0800

    Basic form setup

---
+++ app/components/uuidInput/uuidInput.tsx
@@ -0,0 +1,6 @@
+const UuidInput = ({ setUuid }: { setUuid: (uuid: string) => void }) => {
+    return <input type="text" onChange={(e) => setUuid(e.target.value)} pattern="[0-9]{9}" />; 
+}
+
+export default UuidInput
+    
